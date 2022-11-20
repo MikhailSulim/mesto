060d867869315bfa527adf05042e72faa4b96110
Mikhail Sulim <mika-1984@yandex.ru>
2022-11-20 21:25:56 +0300

    fix исправить ошибки после второго ревью

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,11 @@
 const popupElement = document.querySelector(".popup"); // весь попап
 const popupOpenButtonElement = document.querySelector(".profile__edit-button"); // кнопка открытия
 const popupCloseButtonElement = popupElement.querySelector(".popup__close"); // кнопка закрытия
-let popupNameElement = popupElement.querySelector(".popup__name"); // поле ввода имени в попапе
-let docNameElement = document.querySelector(".profile__name"); // поле отображения имени в профиле
-let popupSubtitleElement = popupElement.querySelector(".popup__subtitle"); // поле ввода имени описания в попапе
-let docSubtitleElement = document.querySelector(".profile__subtitle"); // поле отображения описания в профиле
-let formElement = popupElement.querySelector(".popup__content"); // часть попапа с контентом
+const popupNameElement = popupElement.querySelector(".popup__input_field_name"); // поле ввода имени в попапе
+const docNameElement = document.querySelector(".profile__name"); // поле отображения имени в профиле
+const popupSubtitleElement = popupElement.querySelector(".popup__input_field_subtitle"); // поле ввода имени описания в попапе
+const docSubtitleElement = document.querySelector(".profile__subtitle"); // поле отображения описания в профиле
+const formElement = popupElement.querySelector(".popup__content"); // часть попапа с контентом
 
 // функции //
 const openPopup = function () {
@@ -29,13 +29,7 @@ function formSubmitHandler(evt) {
   closePopup();
 }
 
-const closePopupByClickOverlay = function (event) {
-  // функция закрытия попапа при клике вне формы редактирования
-  if (event.target === event.currentTarget) closePopup();
-};
-
 // обработчики событий //
 popupOpenButtonElement.addEventListener("click", openPopup);
 popupCloseButtonElement.addEventListener("click", closePopup);
-popupElement.addEventListener("click", closePopupByClickOverlay);
 formElement.addEventListener("submit", formSubmitHandler);
