46c265aad734f522463c29017e0bb54c638e653d
Mikhail Sulim <mika-1984@yandex.ru>
2022-12-15 20:21:22 +0300

    refactor: чистка кода, правка описания в readme.md

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,20 +1,20 @@
 const formConfig = {
-  formSelector: '.popup__content',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__save',
-  inactiveButtonClass: 'popup__save_type_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__input-error'
-}
+  formSelector: ".popup__content",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__save",
+  inactiveButtonClass: "popup__save_type_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__input-error",
+};
 
 const enableValidation = (config) => {
-  const {formSelector, inputSelector, submitButtonSelector, ...restConfig} = config;
+  const { formSelector, inputSelector, submitButtonSelector, ...restConfig } =
+    config;
   const forms = [...document.querySelectorAll(formSelector)]; // приведение списка всех найденных форм в попапах к массиву
 
   forms.forEach((form) => {
     const inputs = [...form.querySelectorAll(inputSelector)]; // приведение списка всех найденных инпутов в попапах к массиву
     const button = form.querySelector(submitButtonSelector); // поиск кнопки сохранить на форме
-
 
     form.addEventListener("submit", (evt) => {
       evt.preventDefault();
@@ -27,13 +27,14 @@ const enableValidation = (config) => {
       });
     });
   });
-}
+};
 
-function clearError(popup) { // функция очистки ошибок валидации в случае, если попап был закрыт без сохранения
+function clearError(popup) {
+  // функция очистки ошибок валидации в случае, если попап был закрыт без сохранения
   const inputs = [...popup.querySelectorAll(`.${formConfig.inputErrorClass}`)];
   const errors = [...popup.querySelectorAll(`.${formConfig.errorClass}`)];
   inputs.forEach((input) => {
-    input.classList.remove(formConfig.inputErrorClass); // убрать стиль ошибки для инпута
+    input.classList.remove(formConfig.inputErrorClass);
   });
 
   errors.forEach((error) => {
